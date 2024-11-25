703e25c5de2ca77e135626c66de4917dca193aa0
Pablo Molina <molinapablod@gmail.com>
2024-11-25 15:34:22 +1100

    Add new pages for My Banks, Payments, and Transaction History; implement RightSidebar and Sidebar components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,48 @@
+import MobileNav from "@/components/MobileNav";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = {
+    firstName: "John",
+    lastName: "Doe",
+    $id: "1",
+    email: "john.doe@example.com",
+    userId: "user123",
+    dwollaCustomerUrl: "https://api.dwolla.com/customers/123",
+    dwollaCustomerId: "customer123",
+    address1: "123 Main St",
+    city: "Anytown",
+    state: "CA",
+    zip: "12345",
+    country: "USA",
+    postalCode: "12345",
+    dateOfBirth: "1990-01-01",
+    ssn: "123-45-6789",
+  };
+
   return (
-    <main>
-      {children}
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+      <div className="flex flex-col size-full">
+        <div className="root-layout">
+          <Image
+            src="/icons/logo.svg"
+            width={30}
+            height={30}
+            alt="menu icon"
+          />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { HeaderBox } from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
 import React from 'react'
 
 const Home = () => {
-  const loggedIn = { firstName: 'Pablo' }
+  const loggedIn = { firstName: 'Pablo', lastName: 'Doe', email: 'doe.pablo@mail.com' }
   return (
     <section className='home'>
       <div className='home-content'>
@@ -20,6 +21,11 @@ const Home = () => {
           ></TotalBalanceBox>
         </header>
       </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[]}
+      />
     </section>
   )
 }
