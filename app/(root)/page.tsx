3b7f685aa5ef1a83e42e8f298afeeaaa73f51abe
Pablo Molina <molinapablod@gmail.com>
2024-11-29 13:49:52 +1100

    Implement bank account retrieval and update user profile display; refactor components to use first name and add new bank actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,23 @@
 import { HeaderBox } from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar'
 import TotalBalanceBox from '@/components/TotalBalanceBox'
+import { getAccounts } from '@/lib/actions/bank.actions'
 import { getLoggedInUser } from '@/lib/actions/user.actions'
-import { get } from 'http'
 import React from 'react'
 
-const Home = async () => {
-  const loggedIn = await getLoggedInUser()
+const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
+  const currentPage = Number(page as string) || 1;
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({
+    userId: loggedIn.$id
+  })
+
+  if (!accounts) return;
+
+  const accountsData = accounts?.data;
+  const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
+
+  //const account = await getAccount({ appwriteItemId })
   return (
     <section className='home'>
       <div className='home-content'>
@@ -14,19 +25,19 @@ const Home = async () => {
           <HeaderBox type='greeting'
             title='Welcome'
             subtext='Access and manage your account and transactions efficiently.'
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.firstName || 'Guest'}
           ></HeaderBox>
           <TotalBalanceBox
-            accounts={[]}
-            totalBanks={1}
-            totalCurrentBalance={1250.35}
-          ></TotalBalanceBox>
+            accounts={accountsData}
+            totalBanks={accounts?.totalBanks}
+            totalCurrentBalance={accounts?.totalCurrentBalance}
+          />
         </header>
       </div>
       <RightSidebar
         user={loggedIn}
         transactions={[]}
-        banks={[]}
+        banks={accountsData?.slice(0, 2)}
       />
     </section>
   )
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -12,12 +12,12 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
           <div className='profile-img'
           >
             <span className='text-5xl font-bold text-blue-500'>
-              {user?.name?.charAt(0)}
+              {user?.firstName?.charAt(0)}
             </span>
           </div>
           <div className='profile-details'>
             <h1 className='profile-name'>
-              {user?.name} {user?.lastName}
+              {user?.firstName} {user?.lastName}
             </h1>
             <p className='profile-email'>{user?.email}
             </p>
@@ -46,7 +46,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
               <BankCard 
                 key={banks[0].$id}
                 account={banks[0]}
-                userName={`${user.name} ${user.lastName}`}
+                userName={`${user.firstName} ${user.lastName}`}
                 showBalance={false}
               />
             </div>
@@ -55,7 +55,7 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                 <BankCard 
                   key={banks[1].$id}
                   account={banks[1]}
-                  userName={`${user.name} ${user.lastName}`}
+                  userName={`${user.firstName} ${user.lastName}`}
                   showBalance={false}
                 />
               </div>
